b0650d6582a625b1f012e8d509de56e66f56b737
Mahmoud Marwan <dark1996@live.com>
2024-03-18 20:35:04 +0200

    Feat: Create add product page

--- src/main/java/controllers/commands/admin/AdminCommand.java
+++ src/main/java/controllers/commands/admin/AdminCommand.java
@@ -1,6 +1,5 @@
 package controllers.commands;
 
-import controllers.commands.FrontCommand;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
@@ -13,7 +12,7 @@ public class AdminCommand extends FrontCommand {
     public void process() throws ServletException, IOException {
         System.out.println("Admin Dashboard Command: processing request");
         ServletContext context = request.getServletContext();
-        RequestDispatcher rd = context.getNamedDispatcher(UrlMapping.ADMIN.getServletName());
+        RequestDispatcher rd = context.getNamedDispatcher(UrlMapping.ADMINHOME.getServletName());
         System.out.println("Forwarding Request to Admin Page...");
         rd.forward(request, response);
     }
