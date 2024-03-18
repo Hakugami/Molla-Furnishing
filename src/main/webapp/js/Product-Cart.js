89ba4de7481d222248119f7d3a34e05345347512
Islam Ahmed <62517820+Hakugami@users.noreply.github.com>
2024-03-19 00:32:58 +0200

    cart backend done

--- src/main/webapp/js/Product-Cart.js
+++ src/main/webapp/js/Product-Cart.js
@@ -5,53 +5,54 @@ $(document).ready(function () {
             shoppingData.products.forEach(function (product) {
                 let count = shoppingData.productCounts[product.name] || 1;
                 let productElement = '<tr>' +
-                                        '<td>' +
-                                            '<div class="table-p__box">' +
-                                                '<div class="table-p__img-wrap">' +
-                                                    '<img class="u-img-fluid" src="' + product.images[0] + '" alt="">' +
-                                                '</div>' +
-                                                '<div class="table-p__info">' +
-                                                    '<span class="table-p__name">' +
-                                                        '<a href="product-detail.html">' + product.name + '</a>' +
-                                                    '</span>' +
-                                                    '<span class="table-p__category">' +
-                                                        '<a href="shop-side-version-2.html">' + product.category + '</a>' +
-                                                    '</span>' +
-                                                    '<ul class="table-p__variant-list">' +
-                                                        '<li>' +
-                                                            '<span>Size: ' + product.description + '</span>' +
-                                                        '</li>' +
-                                                        '<li>' +
-                                                            '<span>Color: ' + product.rating + '</span>' +
-                                                        '</li>' +
-                                                    '</ul>' +
-                                                '</div>' +
-                                            '</div>' +
-                                        '</td>' +
-                                        '<td>' +
-                                            '<span class="table-p__price">$' + product.price + '</span>' +
-                                        '</td>' +
-                                        '<td>' +
-                                            '<div class="table-p__input-counter-wrap">' +
-                                                '<div class="input-counter">' +
-                                                    '<span class="input-counter__minus fas fa-minus"></span>' +
-                                                    '<input class="input-counter__text input-counter--text-primary-style" type="text" value="' + count + '" data-min="1" data-max="1000">' +
-                                                    '<span class="input-counter__plus fas fa-plus"></span>' +
-                                                '</div>' +
-                                            '</div>' +
-                                        '</td>' +
-                                        '<td>' +
-                                            '<div class="table-p__del-wrap">' +
-                                                '<a class="far fa-trash-alt table-p__delete-link"></a>' +
-                                            '</div>' +
-                                        '</td>' +
-                                    '</tr>';
+                    '<td>' +
+                    '<div class="table-p__box">' +
+                    '<div class="table-p__img-wrap">' +
+                    '<img class="u-img-fluid" src="' + product.images[0] + '" alt="">' +
+                    '</div>' +
+                    '<div class="table-p__info">' +
+                    '<span class="table-p__name">' +
+                    '<a href="product-detail.html">' + product.name + '</a>' +
+                    '</span>' +
+                    '<span class="table-p__category">' +
+                    '<a href="shop-side-version-2.html">' + product.category + '</a>' +
+                    '</span>' +
+                    '<ul class="table-p__variant-list">' +
+                    '<li>' +
+                    '<span>Size: ' + product.description + '</span>' +
+                    '</li>' +
+                    '<li>' +
+                    '<span>Color: ' + product.rating + '</span>' +
+                    '</li>' +
+                    '</ul>' +
+                    '</div>' +
+                    '</div>' +
+                    '</td>' +
+                    '<td>' +
+                    '<span class="table-p__price">$' + product.price + '</span>' +
+                    '</td>' +
+                    '<td>' +
+                    '<div class="table-p__input-counter-wrap">' +
+                    '<div class="input-counter">' +
+                    '<span class="input-counter__minus fas fa-minus"></span>' +
+                    '<input class="input-counter__text input-counter--text-primary-style" type="text" value="' + count + '" data-min="1" data-max="1000">' +
+                    '<span class="input-counter__plus fas fa-plus"></span>' +
+                    '</div>' +
+                    '</div>' +
+                    '</td>' +
+                    '<td>' +
+                    '<div class="table-p__del-wrap">' +
+                    '<a class="far fa-trash-alt table-p__delete-link"></a>' +
+                    '</div>' +
+                    '</td>' +
+                    '</tr>';
 
                 $('.table-p tbody').append(productElement);
             });
         } else {
         }
     }
+
     loadProductsFromSessionStorage();
 });
 
@@ -79,20 +80,22 @@ $(document).on('click', '.input-counter__plus', function () {
     updateTotalSum();
 });
 
-$(document).on('click', '.table-p__delete-link', function() {
-    let productName = $(this).closest('tr').find('.table-p__name a').text();
-    
-    shopping_products = shopping_products.filter(product => product.name !== productName);
-    let shoppingData = {
-        products: shopping_products,
-        productCounts: productCounts
-    };
+$(document).on('click', '.table-p__delete-link', function () {
+    let productRow = $(this).closest('tr');
+    let productName = productRow.find('.table-p__name a').text();
+
+    // Remove the product from shoppingData.products array
+    let shoppingData = JSON.parse(sessionStorage.getItem('shoppingData'));
+    let updatedProducts = shoppingData.products.filter(product => product.name !== productName);
+    shoppingData.products = updatedProducts;
     sessionStorage.setItem('shoppingData', JSON.stringify(shoppingData));
-    
-    $(this).closest('tr').remove();
-    
+
+    // Remove the product from the table
+    productRow.remove();
+
     updateTotalSum();
 });
+
 
 
 function updateProductCount(input, count) {
@@ -113,8 +116,8 @@ function updateTotalSum() {
     let totalSum = 0;
 
     if (shoppingProducts && shoppingProducts.length > 0) {
-        shoppingProducts.forEach(function(product) {
-            let count = productCounts[product.name] || 1; 
+        shoppingProducts.forEach(function (product) {
+            let count = productCounts[product.name] || 1;
             totalSum += product.price * count;
         });
     }
@@ -126,11 +129,11 @@ function updateTotalSum() {
 updateTotalSum();
 
 function clearShoppingCart() {
-    shopping_products = []; 
-    productCounts = {}; 
+    shopping_products = [];
+    productCounts = {};
     sessionStorage.removeItem('shoppingData');
 
-    $('.table-p tbody').empty(); 
+    $('.table-p tbody').empty();
 }
 
 $(document).on('click', '.route-box__link', function (event) {
