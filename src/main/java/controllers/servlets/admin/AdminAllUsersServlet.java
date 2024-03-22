65638f55e7101e093b60a16fb92390a8503a6419
Mohamed Nofal <nofal.a.mohamed@gmail.com>
2024-03-22 04:23:39 +0200

    implement pagination to display users on the 'All Users' page in the admin section

--- src/main/java/controllers/servlets/admin/AdminAllUsersServlet.java
+++ src/main/java/controllers/servlets/admin/AdminAllUsersServlet.java
@@ -1,16 +1,50 @@
 package controllers.servlets.admin;
 
+import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.DTOs.UserDto;
+import services.UserService;
 import urls.enums.UrlMapping;
 
 import java.io.IOException;
+import java.util.List;
 
 public class AdminAllUsersServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("AdminAllUsersServlet: processing request");
+
+        List<UserDto> users = getUsersDto(req);
+        req.setAttribute("users", users);
+
         req.getRequestDispatcher(UrlMapping.ADMINALLUSERS.getPageName()).forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("AdminAllUsersServlet-doPost: processing request");
+
+        List<UserDto> products = getUsersDto(req);
+
+        // Convert the list of ProductDto objects to JSON
+        String json = new Gson().toJson(products);
+
+        // Write the JSON to the response
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(json);
+    }
+
+    private List<UserDto> getUsersDto(HttpServletRequest req) {
+
+        String page = req.getParameter("currentPage");
+        int pageInt = page != null ? Integer.parseInt(page) : 1;
+        int size = 10;
+
+        UserService userService = new UserService();
+        return userService.getUsers(pageInt, size);
     }
 }
