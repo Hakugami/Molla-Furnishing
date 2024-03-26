796006fd274c0efb51030185c7f76b59b646ad10
Islam Ahmed <62517820+Hakugami@users.noreply.github.com>
2024-03-26 04:00:10 +0200

    checkout address

--- src/main/java/models/entity/Product.java
+++ src/main/java/models/entity/Product.java
@@ -57,7 +57,7 @@ public class Product {
     @Setter
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "product_images", joinColumns = @JoinColumn(name = "product_id"))
-    @Column(name = "image_url",columnDefinition = "LONGTEXT")
+    @Column(name = "image_url",columnDefinition = "TEXT")
     private List<String> images;
 
     @Setter
